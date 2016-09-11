8a36087adcb90e771a30e52bb73be6ec3cd7f18a
Michael Ran <michaelran16@gmail.com>
2016-09-11 23:01:04 +0800

    add pipeline, and planning to integrate with MongoDB

--- scrapy_dygod/pipelines.py
+++ scrapy_dygod/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+
 
 class ScrapyDygodPipeline(object):
+
+    # def __init__(self):
+    #     connection = pymongo.MongoClient(
+    #         settings['MONGODB_SERVER'],
+    #         settings['MONGODB_PORT']
+    #     )
+    #     db = connection[settings['MONGODB_DB']]
+    #     self.collection = db[settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
         return item
--- scrapy_dygod/settings.py
+++ scrapy_dygod/settings.py
@@ -64,9 +64,9 @@ DOWNLOAD_DELAY = 5
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapy_dygod.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapy_dygod.pipelines.ScrapyDygodPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
